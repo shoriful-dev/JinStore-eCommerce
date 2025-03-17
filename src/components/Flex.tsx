acd186c8a325a0c1cc68f1936e69d7ec2d22c646
shoriful-dev <cmshohelrana50@gmail.com>
2025-03-18 02:11:22 +0600

    flex file create

---
+++ src/components/Flex.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface flexProps {
+  className?: string;
+  children: React.ReactNode
+}
+
+const Flex: React.FC<flexProps> = ({className, children}) => {
+  return (
+    <div className={`flex ${className}`}>{children}</div>
+  )
+}
+
+export default Flex
--- src/layouts/Menu.tsx
+++ src/layouts/Menu.tsx
@@ -1,23 +1,74 @@
-import React from 'react'
-import Container from '../components/Container'
+import React from 'react';
+import Container from '../components/Container';
 import Image from '../components/Image';
 import Logo from '../assets/jinStore_logo.png';
+import { IoLocationOutline } from 'react-icons/io5';
+import { LuSearch } from 'react-icons/lu';
+import { FaRegHeart, FaRegUser } from 'react-icons/fa';
+
+const NotificationBadge = ({ count }) => (
+  <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+    {count}
+  </span>
+);
 
 const Menu: React.FC = () => {
   return (
     <nav>
       <Container>
-        <div className="w-[20%]">
-          <div className="">
-            <Image src={Logo} alt='Main Logo'/>
+        {/* Menu Left Part */}
+        <div className="w-[20%] flex items-center py-5 justify-between">
+          <div>
+            <Image src={Logo} alt="Main Logo" />
           </div>
-          <div className=""></div>
+          <div className="flex space-x-4 items-center">
+            <div className="">
+              <IoLocationOutline className="text-2xl" />
+            </div>
+            <div className="w-[60px]">
+              <h3 className="font-Inter text-[11px] text-[#030712]">
+                Deliver to{' '}
+                <span className="font-semibold font-Inter text-[13px]">
+                  all
+                </span>
+              </h3>
+            </div>
+          </div>
         </div>
-        <div className="w-[60%]"></div>
-        <div className="w-[20%]"></div>
+        {/* Menu Middle Part */}
+        <div className="w-[60%] relative">
+          <input
+            type="text"
+            placeholder="Search for products, categories or brands..."
+            className="w-[860px] h-[48px] bg-[#F3F4F6] px-5 text-[#6B7280] rounded-lg outline-none"
+          />
+          <div className="">
+            <LuSearch className="text-2xl absolute top-3 right-[-25px]" />
+          </div>
+        </div>
+        {/* Menu Right Part */}
+        <div className="w-[20%]">
+          <div className="w-[60px] flex space-x-5 items-center">
+            <div>
+              <FaRegUser className="text-2xl" />
+            </div>
+            <h3 className="font-Inter text-[11px] text-[#030712]">
+              Sign In{' '}
+              <span className="font-Inter text-[13px] font-semibold">
+                Account
+              </span>
+            </h3>
+          </div>
+          <div className="">
+            <div className="">
+              <FaRegHeart className='text-2xl'/>
+              <NotificationBadge count={0}/>
+            </div>
+          </div>
+        </div>
       </Container>
     </nav>
   );
-}
+};
 
-export default Menu
+export default Menu;
