3fda0733059794ffcff83fb54737ba859a0b7eef
shoriful-dev <cmshohelrana50@gmail.com>
2025-03-26 01:09:36 +0600

    Refactor Banner component to enable slider functionality

--- src/layouts/Banner.tsx
+++ src/layouts/Banner.tsx
@@ -9,45 +9,45 @@ import Banner2 from '../assets/banner2.png'
 
 
 const Banner: React.FC = () => {
-  // const settings = {
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 2000,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: true,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 600,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2,
-  //         initialSlide: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //       },
-  //     },
-  //   ],
-  // };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <section>
       <Container className="mt-5">
-        {/* <Slider {...settings}>
+        <Slider {...settings}>
           <div className="w-full h-[490px] outline-none">
             <Image src={Banner1} alt="Banner Image" />
           </div>
@@ -60,7 +60,7 @@ const Banner: React.FC = () => {
           <div className="w-full h-[490px] outline-none">
             <Image src={Banner2} alt="Banner Image" />
           </div>
-        </Slider> */}
+        </Slider>
       </Container>
     </section>
   );
