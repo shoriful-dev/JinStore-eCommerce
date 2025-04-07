4010eebc411f7d2b32d529cd609930fa23356504
MD Shoriful Islam <cmshohelrana50@gmail.com>
2025-04-07 09:07:39 +0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Headlng from '../layouts/Headlng'
+import Headlng from '../layouts/Headlng';
 import Nav_Top from '../layouts/Nav_Top'
 import Menu from '../layouts/Menu'
 import Navber from '../layouts/Navber'
