2ac38532c390cc646e76b9dd5db89254d97f11b5
MD Shoriful Islam <cmshohelrana50@gmail.com>
2025-04-08 10:01:01 +0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Headlng from '../layouts/Headlng';
 import Nav_Top from '../layouts/Nav_Top';
 import Menu from '../layouts/Menu';
-import Navber from '../layouts/Navber'
+import Navber from '../layouts/Navber';
 import Banner from '../layouts/Banner'
 
 const Home: React.FC = () => {
