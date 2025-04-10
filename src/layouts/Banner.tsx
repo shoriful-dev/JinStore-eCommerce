b37d6ebc4083d49b85ea35ae02190e5217dfdba0
shoriful-dev <cmshohelrana50@gmail.com>
2025-04-10 12:26:17 +0600

    changes

--- src/layouts/Banner.tsx
+++ src/layouts/Banner.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Container from '../components/Container'
+import React from 'react';
+import Container from '../components/Container';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
