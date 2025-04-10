a876167ed43138e20aed3d1b638755018bd9659e
shoriful-dev <cmshohelrana50@gmail.com>
2025-04-10 12:26:58 +0600

    some changes

--- src/layouts/Banner.tsx
+++ src/layouts/Banner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from '../components/Container';
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick'
 import Image from '../components/Image'
 import Banner1 from '../assets/banner1.png'
